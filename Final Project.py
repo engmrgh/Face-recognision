c3c09d90cf0bb9386185e5eda2112af88c8727f4
eng.mrgh <mohammadrezaghofrany@gmail.com>
2019-01-24 09:33:28 +0330

    A little improvement plus some new implementation such as I have added mean image and subtracted form mean image parts

--- Final Project.py
+++ Final Project.py
@@ -134,25 +134,75 @@ for i in range(5, 130, 25):
 # Section 3 Face recognition with SVD
 
 
+def get_matrix_X():
+    training_data_path = "./training data/"
+    X = []
+    for file in listdir(training_data_path):
+        X.append(numpy.asarray(Image.open(training_data_path + file)))
+    return X
+
+
+def save_mean_img(mean_img):
+    imge = Image.fromarray(mean_img)
+    if imge.mode != 'L':
+        imge = imge.convert('L')
+    imge.save("./Constructed Photos/Section 5/mean image/mean image.png")
+
+
+def calculate_mean_image_and_store_it(X):
+    mean_img = numpy.zeros((IMAGE_HEIGHT, IMAGE_WIDTH))
+    for matrix in X:
+        mean_img = mean_img + matrix
+    mean_img = (1/len(X)) * mean_img
+    return mean_img
+
+
+def get_subtract_from_mean_matrix(X, mean_img):
+    subtracted_matrix = []
+    for img in X:
+        subtracted_matrix.append(img - mean_img)
+    return subtracted_matrix
+
+
+def save_subtracted_from_mean_matrix(subtracted_matrix):
+    for img, i in zip(subtract_matrix, range(len(subtracted_matrix))):
+        img = Image.fromarray(img)
+        if img.mode != 'L':
+            img = img.convert('L')
+            img.save("./Constructed Photos/Section 5/subtracted images/" + str(i) + ".png")
+
+
 def get_matrix_s():
     training_data_path = "./training data/"
-    S = []
+    S = numpy.zeros((IMAGE_WIDTH * IMAGE_HEIGHT, 1))
     for file in listdir(training_data_path):
-        a = numpy.asarray(Image.open(training_data_path + file)).flatten()
-        S.append(a)
+        tmp = numpy.asarray(Image.open(training_data_path + file)).flatten()
+        S = numpy.column_stack((S, tmp))
     return S
 
 
 def get_matrix_F_bar(S):
     f_bar = numpy.zeros((IMAGE_WIDTH * IMAGE_HEIGHT, 1))
     S_2 = []
-    for f in S:
+    for f in S.transpose():
         tmp = (1/len(S)) * f
         S_2.append(tmp)
-    f_bar = S_2[0] + S_2[1] + S_2[2] + S_2[3] + S_2[4] + S_2[5]
+    f_bar = S_2[0] + S_2[1] + S_2[2] + S_2[3] + S_2[4] + S_2[5] + S_2[6] + S_2[7] + S_2[8] + S_2[9] + S_2[10] + S_2[11] + S_2[12] + S_2[13] + S_2[14] + S_2[15] + S_2[16]
     return f_bar
 
 
+def get_matrix_A(f_bar, S):
+    A = []
+    for f in S.transpose():
+        A.append(f - f_bar)
+    return A
+
+
+matrix_X = get_matrix_X()
+matrix_mean_img = calculate_mean_image_and_store_it(matrix_X)
+save_mean_img(matrix_mean_img)
+subtracted_from_mean_matrix = get_subtract_from_mean_matrix(matrix_X, matrix_mean_img)
+save_subtracted_from_mean_matrix(subtracted_from_mean_matrix)
 matrix_S = get_matrix_s()
 matrix_F_bar = get_matrix_F_bar(matrix_S)
-
+matrix_A = get_matrix_A(matrix_F_bar, matrix_S)
